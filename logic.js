531f9f97b773e5840e8a8b26f038e3c90e7e661f
Ananya <ananyaananyaa124@gmail.com>
2024-10-17 10:29:09 +0530

    new

--- logic.js
+++
@@ -1,6 +0,0 @@
-var rand1 = Math.floor(Math.random() + 6) + 1;
-var ranimg = "dice" + rand1 + ".png";
-var imgsrc = "images/" + ranimg;
-var img1 =document.querySelectorAll("img")[0];
-img1.setAttribute("src",ranimg);
-document.querySelectorAll("img")[1].setAttribute("src",ranimg)
